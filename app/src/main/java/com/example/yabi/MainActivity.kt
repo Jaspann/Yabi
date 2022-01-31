36a0c8cd457e8f613a4ad8151914f8c86529c7cd
Jaspann <wiparker2020@gmail.com>
2022-01-31 13:46:16 -0500

    Adds Nav Menu on home page

--- app/src/main/java/com/example/yabi/MainActivity.kt
+++ app/src/main/java/com/example/yabi/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.yabi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawer_layout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
     }
 }
