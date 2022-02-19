4105072eb087d888e3b4986f3bedf34aee8d3609
DeFishe <darian.edward.fisher@outlook.com>
2022-02-19 11:31:47 -0500

    Created getPassword function that returns a password when given an email. Created instrumented test for getPassword function.

--- app/src/androidTest/java/com/example/yabi/FirebaseHelperTest.kt
+++ app/src/androidTest/java/com/example/yabi/FirebaseHelperTest.kt
@@ -8,16 +8,34 @@ import org.junit.Test
 class FirebaseHelperTest {
 
     @Test
-    fun authenticateUser() {
-
-
+    fun getPassword() {
+        val db = Firebase.firestore
+        val testFirebaseHelper = FirebaseHelper(db)
+        val result = testFirebaseHelper.getPassword("darian.fisher@snhu.edu")
+        //while(!testFirebaseHelper.previousTaskFinished) {}
+        while(!testFirebaseHelper.previousTaskFinished) {}
+        assert(testFirebaseHelper.previousTaskSuccess)
+        testFirebaseHelper.resetStatusFlags()
     }
 
     @Test
     fun createUser() {
-        var db = Firebase.firestore
-        var testFirebaseHelper = FirebaseHelper(db)
+        val db = Firebase.firestore
+        val testFirebaseHelper = FirebaseHelper(db)
         testFirebaseHelper.createUser("darian.fisher@snhu.edu", "Darian Fisher", "DJANGO")
+        while(!testFirebaseHelper.previousTaskFinished) {}
+        assert(testFirebaseHelper.previousTaskSuccess)
+        testFirebaseHelper.resetStatusFlags()
+    }
+
+    @Test
+    fun createListing() {
+        val db = Firebase.firestore
+        val testFirebaseHelper = FirebaseHelper(db)
+        testFirebaseHelper.createListing("Used Socks", 10, true,
+            10, "Your finest used socks please and thank you.",
+            "12 Fairview Ln", "Laketown", "Vermont",
+        "USA", 12345)
         while(!testFirebaseHelper.previousTaskFinished) {}
         assert(testFirebaseHelper.previousTaskSuccess)
         testFirebaseHelper.resetStatusFlags()
