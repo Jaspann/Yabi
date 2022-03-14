f6f1e0a42142989e9752fdadc9dfeabf326a1416
Jaspann <wiparker2020@gmail.com>
2022-03-13 23:35:34 -0400

    Continued chat activity

--- app/src/main/java/com/example/yabi/PostChat.kt
+++ app/src/main/java/com/example/yabi/PostChat.kt
@@ -1,11 +1,36 @@
 package com.example.yabi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class PostChat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_chat)
+        setSupportActionBar(toolbar)
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.info_button -> {
+                val intent = Intent(this, ChatPostInfo::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.chat_nav_bar, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
